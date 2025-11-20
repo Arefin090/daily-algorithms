fdf9b2d6eda8745065a14c3bb5844e1c8059c106
Arefin N. Sojol <101785779+Arefin090@users.noreply.github.com>
2025-11-20 16:04:33 +0000

    Refactor G Topological Sort implementation

--- problems/2025-11-06_g-topological-sort/solution.py
+++ problems/2025-11-06_g-topological-sort/solution.py
@@ -17,14 +17,12 @@ graph = [[1, 4], [2, 4], [3], [], [], [4], [2, 7], [3], []]
 visited = [0 for x in range(len(graph))]
 stack = []
 
-
 def print_stack(stack, clothes):
     order = 1
     while stack:
         current_clothing = stack.pop()
         print(order, clothes[current_clothing])
         order += 1
-
 
 def depth_first_search(u, visited, graph):
     visited[u] = 1
@@ -34,12 +32,10 @@ def depth_first_search(u, visited, graph):
 
     stack.append(u)
 
-
 def topological_sort(graph, visited):
     for v in range(len(graph)):
         if not visited[v]:
             depth_first_search(v, visited, graph)
-
 
 if __name__ == "__main__":
     topological_sort(graph, visited)
